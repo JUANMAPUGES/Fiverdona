fc7b057553ecd9ce1f5bd196d78155dfcd8fd76f
cesar <doncesarmercado@hotmail.com>
2023-07-15 13:17:09 +0200

    input search

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,21 @@
-import SearchForm from "../../components/shared/SearchForm/SearchForm";
-function Home() {
-  return (
-    <section>
-      <h1> Lista de services</h1>
-      <SearchForm />
-    </section>
-  );
-}
+import useServices from "../../hooks/useServices";
+import "./home.css";
 
-export default Home;
+const ServiceSearch = () => {
+  const { services } = useServices();
+  return (
+    <main className="serviceSearch">
+      <h2> Lista de servicios</h2>
+      <ul>
+        {services.length > 0 ? (
+          services.map((services) => {
+            return <li key={services.id}>{services.title}</li>;
+          })
+        ) : (
+          <li>¡No se ha encontrado ningún servicio!</li>
+        )}
+      </ul>
+    </main>
+  );
+};
+export default ServiceSearch;
