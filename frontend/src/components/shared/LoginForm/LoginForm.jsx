ea53fd433996926fcb0c6a8390a540b961d1d59e
Juanma Pugés <juanma@MacBook-Pro-de-Maria.local>
2023-07-17 17:20:42 +0200

    login

---
+++ frontend/src/components/shared/LoginForm/LoginForm.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import loginUtility from '../../../utilities/loginUtility';
+//import Spinner from '../Spinner/Spinner';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+
+import './loginForm.css';
+
+const LoginForm = ({ login }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    // Función que maneja el envío del formulario.
+    const handleSubmit = async (e) => {
+        try {
+            e.preventDefault();
+
+            setLoading(true);
+
+            const token = await loginUtility(email, password);
+
+            // Guardamos el token en el localStorage.
+            login(token);
+        } catch (err) {
+            setErrMsg(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
+
+            <label htmlFor='email'>Email:</label>
+            <input
+                type='email'
+                id='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+            />
+            <label htmlFor='password'>Contraseña:</label>
+            <input
+                type='password'
+                id='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                minLength='8'
+                maxLength='100'
+                required
+            />
+            <button>Loguearse</button>
+
+            {loading && <p>Cargando...</p>}
+
+            {errMsg && <ErrorMessage msg={errMsg} />}
+        </form>
+    );
+};
+
+LoginForm.propTypes = {
+    login: PropTypes.func,
+};
+
+export default LoginForm;
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,9 +1,18 @@
-function Login() {
-  return (
-    <main>
-      <h1> Esto es el Login</h1>
-    </main>
-  );
-}
+import { Navigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import LoginForm from '../../components/shared/LoginForm/LoginForm';
+
+const Login = () => {
+    const { token, login } = useAuth();
+
+    // Si la persona está logeada la redirigimos a la página principal.
+  if (token) return <Navigate to='/' />;
+    /******** También tendrá que ir al dashboard con token *********/
+    return (
+        <main className='login'>
+            <LoginForm login={login} />
+        </main>
+    );
+};
 
 export default Login;
