3fb66805dc647200c628941e76fa600e49f5e779
Juanma Pugés <juanma@MacBook-Pro-de-Maria.local>
2023-07-18 18:15:02 +0200

    logout resuelto bug

--- frontend/src/components/shared/Header/Header.jsx
+++ frontend/src/components/shared/Header/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
               <NavLink to="/profile">Perfil</NavLink>
             </div>
             <div className="button" onClick={() => logout()}>
-              <p>Cerrar Sesión</p>
+            <NavLink to="/">Cerrar Sesión</NavLink>
             </div>
           </>
         )}
