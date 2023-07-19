3d146366ade1e1ed7826de2d5859c558c0afade8
Juanma Pugés <juanma@MacBook-Pro-de-Maria.local>
2023-07-19 10:48:37 +0200

    home con token

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import useServices from "../../hooks/useServices";
 import SearchForm from "../../components/shared/SearchForm/SearchForm";
+import { NavLink } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 import "./home.css";
 
 const ServiceSearch = () => {
-  const { token,services, searchParams, setSearchParams } = useServices();
+  const { services, searchParams, setSearchParams } = useServices();
+  const { token } = useAuth();
   return (
     <main className="serviceSearch">
       <h2> Lista de servicios</h2>
@@ -26,13 +29,27 @@ const ServiceSearch = () => {
       )}
       
       {token && (
-          <>
+        <>
+          <SearchForm
+            searchParams={searchParams}
+            setSearchParams={setSearchParams} />
+          <ul>
+            {services.length > 0 ? (
+              services.map((services) => {
+                return <li key={services.id}>{services.title}</li>;
+              })
+            ) : (
+              <li>¡No se ha encontrado ningún servicio!</li>
+            )}
+          </ul>
+          <nav>
             <div className="button">
               <NavLink to="/profile">Añadir un servicio</NavLink>
               ******** CAMBIAR RUTA ********
             </div>
-            
-          </>
+
+          </nav>
+        </>
         
       )}
       
