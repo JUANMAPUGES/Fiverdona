21bf38810cac94f5a1ff7730cfc2571ab2da338a
Juanma Pugés <juanma@MacBook-Pro-de-Maria.local>
2023-07-19 13:04:32 +0200

    home con etiquetas

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,25 +2,41 @@ import useServices from "../../hooks/useServices";
 import SearchForm from "../../components/shared/SearchForm/SearchForm";
 import { NavLink } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
-import "./home.css";
+import Service from "../../components/shared/Service/Service"
+import Spinner from "../../components/shared/Spinner/Spinner"
+import "./home.css"
 
 const ServiceSearch = () => {
-  const { services, searchParams, setSearchParams } = useServices();
+  const { services, resolvedService, searchParams, setSearchParams,errMsg, loading, } = useServices();
   const { token } = useAuth();
 
   return (
-    <main className="serviceSearch">
+    <main className='serviceSearch'>
       <h2> Lista de servicios</h2>
       {!token && (
         <>
           <SearchForm
             searchParams={searchParams}
             setSearchParams={setSearchParams}
+            loading={loading}
           />
+
+          {loading && <Spinner />}
+          {errMsg && <Errormessage msg={errMsg} />}
+          
           <ul>
             {services.length > 0 ? (
               services.map((services) => {
-                return <li key={services.id}>{services.title}</li>;
+                return (
+                  <Service
+                  key={service.id}
+                  service={service}
+                  resolvedService={resolvedService}
+                  loading={loading}
+                  />
+                
+                );
+                
               })
             ) : (
               <li>¡No se ha encontrado ningún servicio!</li>
@@ -31,10 +47,16 @@ const ServiceSearch = () => {
 
       {token && (
         <>
+
           <SearchForm
             searchParams={searchParams}
             setSearchParams={setSearchParams}
+            loading={loading}
           />
+
+          {loading && <Spinner />}
+          {errMsg && <Errormessage msg={errMsg} />}
+          
           <ul>
             {services.length > 0 ? (
               services.map((services) => {
@@ -46,10 +68,15 @@ const ServiceSearch = () => {
           </ul>
           <nav>
             <div className="button">
-              <NavLink to="/services">Añadir un servicio</NavLink>
+             
+             <NavLink to="/services">Añadir un servicio</NavLink>
+              
             </div>
+
           </nav>
         </>
+        
+
       )}
     </main>
   );
