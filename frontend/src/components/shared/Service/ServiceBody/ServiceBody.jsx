6e5383ee02972dc6076ccd69033d03549b21b9e0
Fatima <fatimameddahg@hotmail.com>
2023-08-01 19:23:53 +0200

    correciones

--- frontend/src/components/shared/Service/ServiceBody/ServiceBody.jsx
+++ frontend/src/components/shared/Service/ServiceBody/ServiceBody.jsx
@@ -1,5 +1,4 @@
-import PropTypes from "prop-types";
-import CommentService from "../../Comment/CommentService";
+import PropTypes from 'prop-types';
 
 const ServiceBody = ({ service }) => {
   return (
@@ -7,24 +6,6 @@ const ServiceBody = ({ service }) => {
       <div>
         <p>{service.description}</p>
       </div>
-      <ul className="commentList">
-        {service.comments?.length > 0 ? (
-          service.comments.map((comment) => {
-            return (
-              <CommentService
-                key={comment.id}
-                username={comment.username}
-                comment={comment}
-                createdAt={comment.createdAt}
-                text={comment.text}
-                filename={comment.fileName}
-              />
-            );
-          })
-        ) : (
-          <li>¡De momento no hay comentarios asociados a este servicio!</li>
-        )}
-      </ul>
     </>
   );
 };
